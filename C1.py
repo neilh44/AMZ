e81643433b6d1a499b334794a60d9c8e127dec7a
neilh44 <nilesh.hanotia@outlook.com>
2024-04-17 22:05:35 +0530

    Update C1.py

--- C1.py
+++ C1.py
@@ -30,7 +30,7 @@ def main():
     st.title("Language Model Text Generation with Streamlit")
 
     # Load the CSV file
-    csv_file_path = "https://raw.githubusercontent.com/neilh44/AMZ/main/A1.csv"
+    csv_file_path = "https://raw.githubusercontent.com/neilh44/AMZ/main/b2.csv"
     df = load_csv(csv_file_path)
 
     # Display the CSV file (optional)
