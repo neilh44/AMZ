78557d8f00abfc94ee59b60efbb4de6c2b12e342
neilh44 <nilesh.hanotia@outlook.com>
2024-04-17 23:46:26 +0530

    Update C1.py

--- C1.py
+++ C1.py
@@ -43,7 +43,6 @@ def load_mistral_model():
         tokenizer_name=MISTRAL_7B,
         model_name=MISTRAL_7B,
         device_map="auto",
-        model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": True},
     )
 
     # Define the embedding model
