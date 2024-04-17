a25839448aa5329741693763016ad1e30a05dee7
neilh44 <nilesh.hanotia@outlook.com>
2024-04-17 09:19:08 +0530

    Update C1.py

--- C1.py
+++ C1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from llama_index import Llama_Index
+from llama_index import llama_Index
 
 
 # Load the model and tokenizer
